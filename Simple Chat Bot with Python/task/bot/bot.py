ea0cf64740b99e3732ed95d53f44c8d4949752a9
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-04-24 12:24:00 +0200

    Stage 4/5

--- Simple Chat Bot with Python/task/bot/bot.py
+++ Simple Chat Bot with Python/task/bot/bot.py
@@ -8,10 +8,21 @@ print('What a great name you have, ' + name + '!')
 print('Let me guess your age.')
 print('Enter remainders of dividing your age by 3, 5 and 7.')
 
-# reading all remainders
-remainder3 = int(input()) % 3
-remainder5 = int(input()) % 5
-remainder7 = int(input()) % 7
-your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+rem3 = int(input())
+rem5 = int(input())
+rem7 = int(input())
 
-print(f"Your age is {your_age}; that's a good time to start programming!")
+age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+print('Now I will prove to you that I can count to any number you want.')
+
+# read a number and count to it here
+number = int(input())
+i = 0
+while i <= number:
+    print(f"{i} !")
+    i += 1
+
+print('Completed, have a nice day!')
+
