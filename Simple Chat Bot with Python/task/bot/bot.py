623cdbdbfa4323bf22b69baf822771db21d0df2f
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-04-24 12:22:57 +0200

    Stage 3/5

--- Simple Chat Bot with Python/task/bot/bot.py
+++ Simple Chat Bot with Python/task/bot/bot.py
@@ -2,7 +2,16 @@ print('Hello! My name is Aid.')
 print('I was created in 2020.')
 print('Please, remind me your name.')
 
-# reading a name
-your_name = input()
+name = input()
 
-print(f'What a great name you have, {your_name}!')
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+# reading all remainders
+remainder3 = int(input()) % 3
+remainder5 = int(input()) % 5
+remainder7 = int(input()) % 7
+your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+print(f"Your age is {your_age}; that's a good time to start programming!")
