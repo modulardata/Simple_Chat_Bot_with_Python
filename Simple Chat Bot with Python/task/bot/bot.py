1fe5ff4dfe171e0e55a2c4f89b20c5fe7044adc0
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-04-24 12:22:07 +0200

    Stage 2/5

--- Simple Chat Bot with Python/task/bot/bot.py
+++ Simple Chat Bot with Python/task/bot/bot.py
@@ -1,3 +1,8 @@
-# write your code here
 print('Hello! My name is Aid.')
-print('I was created in 2020')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+# reading a name
+your_name = input()
+
+print(f'What a great name you have, {your_name}!')
